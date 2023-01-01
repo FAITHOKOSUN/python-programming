fd80519f96314d3a733befcb8c58064b2508ca5c
FAITHOKOSUN <okosunfaith2@gmail.com>
2023-01-01 22:06:49 +0000

    task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >= ord('a') <= ord(i) <= ord('z'):
-            i = chr(ord(i) - (ord('a') - ord('A')))
-            print("{:s}".format(i),end ='')
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i),end ='')
 
     print("")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    num = number * abs(1/number)
-lastdg = str(number)[-1]
-lastdg = int(num*int(lastdg))
-print (lastdg)
+    if number != 0:
+        lastdg = str(number)[-1]
+        lastdg = int(num*int(lastdg))
+        
+    else:
+        lastdg = 0
+
+    print (lastdg)
+
+
