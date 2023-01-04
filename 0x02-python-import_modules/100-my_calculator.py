99ec4e06d2e09dcc78addaa7bc68c9b6898004d6
Adebayo Jubreel <whizkidjubrealguy.com>
2023-01-04 22:41:40 +0000

    changed line 2 to __main__

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__name__":
+if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
-    #count = len(argv) - 1
+    count = len(argv) - 1
     
-
-    #if count != 3:
-    if len(argv) != 4:
+    if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
