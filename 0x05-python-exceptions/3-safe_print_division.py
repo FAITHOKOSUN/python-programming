9b5df0cfd940f9f7eaaf9859050b1453112b88d8
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-01-17 20:57:48 +0000

    exceptions in python

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/usr/python3
+def safe_print_division(a, b):
+    try:
+        result = a/b
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
