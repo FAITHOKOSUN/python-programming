2addced76bb5da970ef324ed1408161e1bc822b5
hiphotler <oluwakorede.oladetohun180211041@st.lasu.edu.ng>
2023-03-16 22:58:30 +0100

    N queens

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,62 @@
+class Rectangle:
+
+    """
+    Define a rectangle based on 0-rectangle.py
+    """
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        per = 2 * (self.__width + self.__height)
+        if self.__width ==0 or self.__height ==0:
+            return 0
+        return per
+
+    def __str__(self):
+        str_repr = ""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for j in range(self.__height):
+            for k in range(self.__width):
+                str_repr += type(self).print_symbol
+            if j != self.__height - 1:
+                str_repr += '\n'
+        return str_repr
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,72 @@
+class Rectangle:
+
+    """
+    Define a rectangle based on 0-rectangle.py
+    """
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1,Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2,Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        per = 2 * (self.__width + self.__height)
+        if self.__width ==0 or self.__height ==0:
+            return 0
+        return per
+
+    def __str__(self):
+        str_repr = ""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for j in range(self.__height):
+            for k in range(self.__width):
+                str_repr += type(self).print_symbol
+            if j != self.__height - 1:
+                str_repr += '\n'
+        return str_repr
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
